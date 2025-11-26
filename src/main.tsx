b9292c69fe74b8a2e3d3c0b4563620a8a587f675
Sam Li <samdleeone@gmail.com>
2025-11-26 21:47:53 +0800

    chore: init project

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { LocaleProvider } from "@douyinfe/semi-ui";
+import zh_CN from "@douyinfe/semi-ui/lib/es/locale/source/zh_CN";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <LocaleProvider locale={zh_CN}>
+      <App />
+    </LocaleProvider>
+  </StrictMode>
+);
+
